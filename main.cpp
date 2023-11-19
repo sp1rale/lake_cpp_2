36aacd38a8b7d5330a8ccc79dc478a3d32446a16
sp1rale <dimayaremchyk24@gmail.com>
2023-11-19 11:59:06 +0200

    done

--- main.cpp
+++ main.cpp
@@ -1,8 +1,34 @@
 #include <iostream>
 using namespace std;	
 
-int main()
-{
-	cout << "Hello World!" << endl;
+void sumArr(const int* arrayA, const int* arrayB, int* arrayC, int size) {
+	for (int i = 0; i < size; ++i) {
+		arrayC[i] = arrayA[i] + arrayB[i];
+	}
+}
+void printArr(const int* array, int size) {
+	for (int i = 0; i < size; ++i) {
+		cout << array[i] << " ";
+	}
+	cout << endl;
+}
+int main() {
+	const int size = 5;
+	int arrayA[size] = { 1, 2, 3, 4, 5 };
+	int arrayB[size] = { 6, 7, 8, 9, 10 };
+
+	int arrayC[size];
+
+	sumArr(arrayA, arrayB, arrayC, size);
+
+	cout << "Array A: ";
+	printArr(arrayA, size);
+
+	cout << "Array B: ";
+	printArr(arrayB, size);
+
+	cout << "Array C: ";
+	printArr(arrayC, size);
+
 	return 0;
 }
